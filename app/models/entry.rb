278c2660add4836e47b86c07dcc776e256fbc090
ahmadrosid <alahmadrosid@gmail.com>
2021-11-07 18:42:15 +0700

    feat: implement food entry ui and tests

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,7 @@
 class Entry < ApplicationRecord
+    validates :meal_type, :calories, :proteins, :carbohydrates, :fats, presence: true
+
+    def day
+        self.created_at.strftime("%b %e, %Y")
+    end
 end
--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with valid attributes" do
+    entry = Entry.new(meal_type: "Breakfast", carbohydrates: 35, proteins: 12, fats: 10, calories: 10)
+    assert entry.save
+  end
+
+  test "should not save entry without meal_type" do 
+    entry = Entry.new(carbohydrates: 35, proteins: 12, fats: 10, calories: 10)
+    assert_not entry.save
+  end
+  
+  test "should not save entry without carbohydrates" do 
+    entry = Entry.new(meal_type: "Breakfast", proteins: 12, fats: 10, calories: 10)
+    assert_not entry.save
+  end
+
+  test "should not save entry without proteins" do 
+    entry = Entry.new(meal_type: "Breakfast", carbohydrates: 35, fats: 10, calories: 10)
+    assert_not entry.save
+  end
+
+  test "should not save entry without fats" do 
+    entry = Entry.new(meal_type: "Breakfast", carbohydrates: 35, proteins: 12, calories: 10)
+    assert_not entry.save
+  end
+
+  test "should not save entry without calories" do 
+    entry = Entry.new(meal_type: "Breakfast", carbohydrates: 35, proteins: 12, fats: 10)
+    assert_not entry.save
+  end
+
 end
